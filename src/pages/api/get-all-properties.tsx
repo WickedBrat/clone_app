ef1dfb8c85fa79d77567c419908167c40aa10931
WickedBrat <harshsrivastav123@gmail.com>
2022-09-18 19:30:09 +0530

    fix

--- src/pages/api/get-all-properties.tsx
+++ src/pages/api/get-all-properties.tsx
@@ -1,9 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiResponse } from 'next';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> {
+export default async function handler(res: NextApiResponse): Promise<void> {
   const response = await fetch(
     'http://3.110.64.51/workflow_API/Api/GetPropertys',
     {
