497a8bbd45bda49695d678bb7df8db92a8ef3552
WickedBrat <harshsrivastav123@gmail.com>
2022-09-19 15:40:39 +0530

    fixes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -363,6 +363,7 @@ export default function Sidebar({
           <button
             className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
             aria-controls="mobile-menu"
+            onClick={() => setSidebarOpen(true)}
           >
             <span className="sr-only">Open sidebar</span>
             <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
